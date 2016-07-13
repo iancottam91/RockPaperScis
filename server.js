598d70535cd186b7c84faef16c1b9649c57a32bb
iancottam91 <icottam@msn.com>
2016-07-13 10:09:23 +0100

    Add node server

---
+++ server.js
@@ -0,0 +1,20 @@
+// 3rd party
+var express = require('express');
+var app = express();
+
+app.use('/', express.static(__dirname + '/'));
+
+/* serves all the static files */
+
+app.get('/', function(req, res){ 
+
+  res.sendFile(__dirname + 'index.html');
+
+});
+
+// redirects at index.html and 404s
+
+
+
+app.listen(process.env.PORT || 3229);
+
